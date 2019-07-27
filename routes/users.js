80ac06baa2220e4967768e512a26184b17eb95df
Ravi kumar <ravikumar.pilla479@gmail.com>
2019-07-28 02:05:11 +0530

    mongoose-populate added

--- routes/users.js
+++ routes/users.js
@@ -16,10 +16,22 @@ router.post('/signup', (req, res, next) => {
         res.json({ err: err });
       }
       else {
-        passport.authenticate('local')(req, res, () => {
-          res.statusCode = 200;
-          res.setHeader('Content-Type', 'application/json');
-          res.json({ success: true, status: 'Registration Successful!' });
+        if (req.body.firstname)
+          user.firstname = req.body.firstname;
+        if (req.body.lastname)
+          user.lastname = req.body.lastname;
+        user.save((err, user) => {
+          if (err) {
+            res.statusCode = 500;
+            res.setHeader('Content-Type', 'application/json');
+            res.json({ err: err });
+            return;
+          }
+          passport.authenticate('local')(req, res, () => {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json({ success: true, status: 'Registration Successful!' });
+          });
         });
       }
     });
@@ -32,9 +44,10 @@ router.post('/login', passport.authenticate('local'), (req, res) => {
   res.json({ success: true, token: token, status: 'You are successfully logged in!' });
 });
 
+//issue
 router.get('/logout', (req, res, next) => {
-  if (req.session) {
-    req.session.destroy();
+
+  if (req.user) {
     res.clearCookie('session-id');
     res.redirect('/');
   }
