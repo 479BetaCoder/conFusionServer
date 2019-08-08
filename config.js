a6b9ad11a4f0aa76ac06ed9811bb8c59dd0a0e13
Ravi Kumar <ravikumar.pilla479@gmail.com>
2019-08-09 02:47:48 +0530

    secured-config

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ module.exports = {
     'secretKey': '12345-67890-09876-54321',
     'mongoUrl': 'mongodb://localhost:27017/conFusion',
     'facebook': {
-        clientId: '403944083558874',
-        clientSecret: '7173f9886eef4fed3ec8c7f7811d7846'
+        clientId: 'your-client-id',
+        clientSecret: 'your-secret-key'
     }
 }
 
